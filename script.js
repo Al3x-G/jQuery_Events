9d57b0a678d18e10634ebbec665512d9aad9a4f2
Alex Gordon <alexg1991@outlook.com>
2024-07-11 15:05:48 +0000

    Add example of method chaining with comments

--- script.js
+++ script.js
@@ -23,10 +23,28 @@ $(document).ready(function() {
         $(".stream3").removeClass('highlight_stream');
         $(".stream2").addClass('highlight_stream');
    });
+
+   /*
+   below is an example of method chaining 
+    */
    $("#stream3_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream3").addClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream').addClass('highlight_stream'); // same as above, cleaner code
    });
+
+   /*
+   The example below is unrelated to this project but is a good example of event method chaining
+
+   $("#mybutton").removeclass("bluebox").addClass("border");
+   $("p").css("color","blue").slideUp(2000).slideDown(2000);
+   $("a").attr("href", "http://www.example.com");
+
+   Here, we have the slideUp() followed by the slideDown() chained together. 
+   An element's attributes can contain useful information, so it's important to be able to access 
+   and manipulate them. The attr() function acts both as a getter and a setter. 
+   As with the CSS function, we can query with one parameter, here we're looking for an href, 
+   and update the attribute for the second parameter, in this case, to example.com
+   */
+
 }); 
